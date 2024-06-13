3bce2decfd4bb6a47ae5a51bbd26496a62762622
unknown <arvindaffa49@gmail.com>
2024-06-14 00:19:27 +0700

    feat:init terraform

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "my_ec2_instance" {
+  ami           = "ami-0be48b687295f8bd6"
+  instance_type = "t3.micro"
+  key_name      = "ec2-1"
+  tags = {
+    Name = "fppso"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt upgrade -y",
+      "docker --version",
+      "docker ps"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/Downloads/ec2-1 (1).pem")
+      host        = aws_instance.my_ec2_instance.public_ip
+    }
+  }
+}
